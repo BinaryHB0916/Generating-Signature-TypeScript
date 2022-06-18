5962916d5864f335330734d4a2da782955cb5e22
Wei Duan (段少) <davidduan0916@gmail.com>
2022-06-18 17:01:26 +0800

    update: hide private key and wording refinement

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,19 @@ async function personalSign(message: Buffer, privateKey: Buffer): Promise<Buffer
 }
 
 async function main() {
+    // this message come from the return attribute "sign_payload" of everytime calling API: v1/proof/payload
     const message = Buffer.from('{\"action\":\"create\",\"created_at\":\"1655456062\",\"identity\":\"binaryhb\",\"platform\":\"twitter\",\"prev\":\"+2uzlxoVZbIHneWjJH4mG+oTpza0eOQgOdyS3qHXqExI8O3hi4BUi1LEDNFUDqhvoZ2KCHY8VRyF6SSwwW4jrgA=\",\"uuid\":\"353449e6-3a6f-4ac8-ae65-ba14bf466baf\"}', 'utf8');
-    // Change this Private Key to your own
-    const secretKey = Buffer.from('43c25fecc20e6b2a0d86c81a0202d125c0181deb9975d1170d80378c7e05b429', 'hex');
+    // ATTENTION! RUN THIS LOCALLY! NEVER SHARE YOUR PRIVATE KEY WITH ANY OTHERS OR PUBLIC!
+    // replace XXX with your own Private Key for generating a signature
+    const secretKey = Buffer.from('XXX', 'hex');
     const signature = await personalSign(message, secretKey);
 
     console.log(`Signature: 0x${signature.toString('hex')}`);
+    // For demo ONLY
     // Signature: 0xf72fe6b00be411bd70ffe1b9bf322f18529ea10e9559dd26ba10387544849fc86d712709dfb709efc3dcc0a01b6f6b9ca98bd48fe780d58921f4926c6f2c0b871b
 
     console.log(`Signature(base64): ${signature.toString('base64')}`);
+    // For demo ONLY
     // Signature(base64): 9y/msAvkEb1w/+G5vzIvGFKeoQ6VWd0muhA4dUSEn8htcScJ37cJ78PcwKAbb2ucqYvUj+eA1Ykh9JJsbywLhxs=
 }
 
